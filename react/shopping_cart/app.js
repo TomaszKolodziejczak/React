efb613e48dd1cc1b4a838aba098a912ee209504f
tomasz <kolodziejczak.tomasz@gmail.com>
2021-07-30 15:41:00 +0200

    shoppingCart

--- react/shopping_cart/app.js
+++ react/shopping_cart/app.js
@@ -16,12 +16,23 @@ class ShoppingCart extends React.Component {
     }))
   }
 
+  handleBuyButton = () => {
+    this.setState({
+      avalilableProducts: this.state.avalilableProducts - this.state.shoppingCart,
+      shoppingCart: 0,
+    })
+  }
+
   render() {
+
+    const style = this.state.shoppingCart === 0 ? { opacity: 0.3 } : {};
+
     return (
       <>
         <button disabled={this.state.shoppingCart ? false : true} onClick={this.handleRemoveFromCart}>-</button>
-        <span>{this.state.shoppingCart}</span>
+        <span style={style}>{this.state.shoppingCart}</span>
         <button disabled={this.state.shoppingCart === this.state.avalilableProducts ? true : false} onClick={this.handleAddToCart}>+</button>
+        {this.state.shoppingCart > 0 && <button onClick={this.handleBuyButton}>Buy</button>}
       </>
     )
   }
