1d538ccaa8e10654f36d3fce7169e6abc5522714
tomasz <kolodziejczak.tomasz@gmail.com>
2021-08-01 11:57:40 +0200

    timekeeper

--- CRA_project_timekeeper/src/components/App.js
+++ CRA_project_timekeeper/src/components/App.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.css'
+import SwitchButton from './SwitchButton';
 
 class App extends Component {
+  state = {
+    time: 0,
+    actitve: false,
+  }
+
+  handleClick = () => {
+    if (this.state.active) {
+      clearInterval(this.idInterval)
+    } else {
+      this.idInterval = setInterval(() => this.addSecond(), 1000)
+    }
+
+    this.setState({
+      active: !this.state.active
+    })
+  }
+
+  addSecond = () => {
+    this.setState({
+      time: this.state.time + 1
+    })
+  }
+
   render() {
     return (
-      <div>App</div>
+      <>
+        <p>{this.state.time}</p>
+        <SwitchButton click={this.handleClick} active={this.state.active} />
+      </>
     );
   }
 }
