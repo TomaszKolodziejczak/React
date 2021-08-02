9ad395e7a5259c078a9e47f1a0add48c4b862ba5
tomasz <kolodziejczak.tomasz@gmail.com>
2021-08-02 18:33:21 +0200

    words

---
+++ project_json_fetch/src/components/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+import Word from './Word';
+
+class App extends Component {
+
+  state = {
+    words: [{
+      "id": 1,
+      "en": "cat",
+      "pl": "kot"
+    },
+    {
+      "id": 2,
+      "en": "dog",
+      "pl": "pies"
+    },
+    {
+      "id": 3,
+      "en": "fish",
+      "pl": "ryba"
+    }]
+  }
+
+  render() {
+    const words = this.state.words.map(word => (
+      <Word />
+    ))
+    return (
+      <ul className="App">
+        {words}
+      </ul>
+    );
+  }
+}
+
+export default App;
---
+++ project_json_fetch/src/components/Word.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Word.css'
+
+const Word = props => (
+    <li>Word</li>
+)
+
+export default Word
