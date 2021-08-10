212f9bf641922a6a9c3dc82624f7b0a0313f30ca
tomasz <kolodziejczak.tomasz@gmail.com>
2021-08-10 17:25:26 +0200

    header

--- Single_page_apps/shop/src/layouts/Header.js
+++ Single_page_apps/shop/src/layouts/Header.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+
+import '../styles/Header.css'
+
+import header1 from '../images/header1.jpg';
+import header2 from '../images/header2.JPG';
+import header3 from '../images/header3.jpg';
 
 const Header = () => {
     return (
-        <div>Hello</div>
+        <>
+            <Switch>
+                <Route path="/" exact render={(() => (
+                    <img src={header1} alt='felek1' />
+                ))} />
+                <Route path="/contact" render={(() => (
+                    <img src={header2} alt='felek2' />
+                ))} />
+                <Route path="/products" render={(() => (
+                    <img src={header3} alt='felek3' />
+                ))} />
+                <Route path="/admin" render={(() => (
+                    <img src={header1} alt='felek1' />
+                ))} />
+                <Route render={(() => (
+                    <img src={header1} alt='felek1' />
+                ))} />
+            </Switch>
+        </>
     );
 }
 
