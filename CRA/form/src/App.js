cb5c1841acde1678c0a0e74155408c5150e8d5e6
tomasz <kolodziejczak.tomasz@gmail.com>
2021-09-21 14:11:53 +0200

    form

--- CRA/form/src/App.js
+++ CRA/form/src/App.js
@@ -7,22 +7,38 @@ class App extends Component {
 
   state = {
     username: '',
-    email: ''
+    email: '',
+    password: '',
+    accept: false
 
   }
 
   handleChange = event => {
-
+    const type = event.target.type;
+    const name = event.target.name;
     const value = event.target.value;
-    this.setState({
-      username: value
-    })
+
+    if (type === "checkbox") {
+      const checked = event.target.checked;
+      this.setState({
+        [name]: checked
+      })
+    } else if (type === "text" || type === "email" || type === "password") {
+      this.setState({
+        [name]: value
+      })
+    }
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log('enter')
   }
 
   render() {
     return (
       <div className="App">
-        <form>
+        <form onSubmit={this.handleSubmit} noValidate>
           <label htmlFor="user">Your name:
             <input
               type="text"
@@ -35,20 +51,30 @@ class App extends Component {
           <label htmlFor="email">Your email:
             <input
               type="email"
-              id="uemailser"
-              name="username"
+              id="email"
+              name="email"
               value={this.state.email}
               onChange={this.handleChange} />
           </label>
-
-          <label htmlFor="user">Your name:
+          <label htmlFor="password">Your password:
             <input
-              type="text"
-              id="user"
-              name="username"
-              value={this.state.username}
+              type="password"
+              id="password"
+              name="password"
+              value={this.state.password}
               onChange={this.handleChange} />
           </label>
+          <label htmlFor="accept">
+            <input
+              type="checkbox"
+              id="accept"
+              name="accept"
+              checked={this.state.accept}
+              onChange={this.handleChange}
+            />
+            I agree on everything
+          </label>
+          <button>Join</button>
         </form>
       </div>
     )
