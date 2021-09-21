be8549b46ae7b9a0cda42075efe6e7907ae5fa10
tomasz <kolodziejczak.tomasz@gmail.com>
2021-09-21 13:43:50 +0200

    form

--- CRA/form/src/App.js
+++ CRA/form/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+
+class App extends Component {
+
+  state = {
+    username: '',
+    email: ''
+
+  }
+
+  handleChange = event => {
+
+    const value = event.target.value;
+    this.setState({
+      username: value
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <label htmlFor="user">Your name:
+            <input
+              type="text"
+              id="user"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange} />
+          </label>
+
+          <label htmlFor="email">Your email:
+            <input
+              type="email"
+              id="uemailser"
+              name="username"
+              value={this.state.email}
+              onChange={this.handleChange} />
+          </label>
+
+          <label htmlFor="user">Your name:
+            <input
+              type="text"
+              id="user"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange} />
+          </label>
+        </form>
+      </div>
+    )
+  }
 }
+
 
 export default App;
