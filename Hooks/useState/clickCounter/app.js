947660d71b2f121ae5169a2882db16072fc5f43f
tomasz <kolodziejczak.tomasz@gmail.com>
2021-09-26 12:42:32 +0200

    clickCounter

---
+++ Hooks/useState/clickCounter/app.js
@@ -0,0 +1,75 @@
+import React, { useState, Fragment } from 'react';
+
+const Counter = props => {
+    const [count, setCount] = useState(0)
+    const [result, setResult] = useState(props.result)
+
+    const handleMathClick = (type, number) => {
+        if (type === 'subtraction') {
+            setCount(count + 1);
+            setResult(result - number);
+        } else if (type === 'addition') {
+            setCount(count + 1);
+            setResult(result + number);
+        } else if (type === 'reset') {
+            setCount(count + 1);
+            setResult(props.result);
+        }
+    }
+
+    return (
+        <Fragment>
+            <MathButton
+                name="-10"
+                number="10"
+                type="subtraction"
+                click={handleMathClick}
+            />
+            <MathButton
+                name="-1"
+                number="1"
+                type="subtraction"
+                click={handleMathClick}
+            />
+            <MathButton
+                name="Reset"
+                type="reset"
+                click={handleMathClick}
+            />
+            <MathButton
+                name="+1"
+                number="1"
+                type="addition"
+                click={handleMathClick}
+            />
+            <MathButton
+                name="+10"
+                number="10"
+                type="addition"
+                click={handleMathClick}
+            />
+            <ResultPanel count={count} result={result} />
+        </Fragment>
+    );
+}
+const MathButton = props => {
+    const number = parseInt(props.number);
+
+    return (
+        <button onClick={() => props.click(props.type, number)}>{props.name}</button>
+    )
+}
+
+const ResultPanel = props => {
+    return (
+        <React.Fragment>
+            <h1>Clicked: {props.count}</h1>
+            <h1>Result: {props.result}</h1>
+        </React.Fragment>
+    )
+}
+
+const startValue = 0;
+
+
+ReactDOM.render(<Counter result={startValue} />, document.getElementById('root'));
